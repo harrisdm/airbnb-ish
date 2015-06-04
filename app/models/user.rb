85e05374f0adc699214a260dfe56e09f192160bb
Mark Harris <harrisdm@hotmail.com>
2015-06-05 02:24:47 +1000

    name stored in 1 field & user name in tables links to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,7 @@ class User < ActiveRecord::Base
   has_many :properties
 
   has_secure_password
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
+  validates :name, :presence => true
   validates :email, :uniqueness => true
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,9 @@ t1 = PropertyType.create :name => "Entire Place"
 t2 = PropertyType.create :name => "Private Room"
 t3 = PropertyType.create :name => "Shared Room"
 
-u1 = User.create :first_name => "Mark", :last_name => "Harris", :email => "harrisdm@hotmail.com", :password_digest =>"$2a$10$4vE0VSa31ygDLYJcWivfl.5CTitod9MpRP.9riAKF5XBXgP3oWS4."
-u2 = User.create :first_name => "Test", :last_name => "User 1", :email => "1@u.com", :password_digest =>"$2a$10$F3IuDGSXg3cwFlU3juX4PuZXInksI6tVTwE0h4hUIUMhQ6w1FY2oi" #123
-u3 = User.create :first_name => "Test", :last_name => "User 2", :email => "2@u.com", :password_digest =>"$2a$10$F3IuDGSXg3cwFlU3juX4PuZXInksI6tVTwE0h4hUIUMhQ6w1FY2oi" #123
+u1 = User.create :name => "Mark Harris", :email => "harrisdm@hotmail.com", :password_digest =>"$2a$10$4vE0VSa31ygDLYJcWivfl.5CTitod9MpRP.9riAKF5XBXgP3oWS4."
+u2 = User.create :name => "Test User 1", :email => "1@u.com", :password_digest =>"$2a$10$F3IuDGSXg3cwFlU3juX4PuZXInksI6tVTwE0h4hUIUMhQ6w1FY2oi" #123
+u3 = User.create :name => "Test User 2", :email => "2@u.com", :password_digest =>"$2a$10$F3IuDGSXg3cwFlU3juX4PuZXInksI6tVTwE0h4hUIUMhQ6w1FY2oi" #123
 
 
 p1 = Property.create :title => "My House", :address => "179 Lord Street, Sydney", :description => "My house bitches", :property_type_id => 1, :rent => 300.0, :beds => 3, :baths => 2, :max_people => 5, :min_stay => 3, :check_in_time => "2000-01-01 16:00:00 UTC", :check_out_time => "2000-01-01 11:00:00 UTC", :pets => false, :user_id => 1, :active => true
